4dee7dbc2b9a0d7957f508819e5af58d30c84912
andrey.lesnov <andreythecoolguy@gmail.com>
2026-04-17 21:57:50 +0300

    refactor: cells storage

--- src/core/hexes_storage.py
+++ src/core/hexes_storage.py
@@ -7,7 +7,7 @@ class CellsStorage:
     R_KM = 7
 
     def __init__(self):
-        self.cells: dict[str, tuple[int, int]] = {}
+        self.cells: set[str] | None = None
 
     def setup(self):
         center = h3.latlng_to_cell(self.LAT, self.LON, self.RES)
@@ -16,20 +16,12 @@ class CellsStorage:
         k = int(self.R_KM / edge_length)
         grid_disk = h3.grid_disk(center, k)
 
-        for h in grid_disk:
-            if h3.great_circle_distance((self.LAT, self.LON), h3.cell_to_latlng(h)) < self.R_KM:
-                h_int = h3.str_to_int(h)
-                base = h_int // 512
+        cells_in_area = {
+            h for h in grid_disk
+            if h3.great_circle_distance((self.LAT, self.LON), h3.cell_to_latlng(h)) < self.R_KM
+        }
 
-                self.cells[h] = (self._get_level(base), self._get_cell_id(base))
-
-    @staticmethod
-    def _get_level(base: int) -> int:
-        return (base % 74) - 120
-
-    @staticmethod
-    def _get_cell_id(base: int) -> int:
-        return (base % 100) + 1
+        self.cells = cells_in_area
 
 
 cells_storage = CellsStorage()
